42d18b9eca8a2a8e2488b6cf2ab34bed828d4418
Jeremiah Almazar <jemalmazar@gmail.com>
2016-03-21 23:46:37 -0700

    Build and style footer for mobile view.

--- wp-content/themes/bigsisters/functions.php
+++ wp-content/themes/bigsisters/functions.php
@@ -84,7 +84,13 @@ add_filter( 'stylesheet_uri', 'red_starter_minified_css', 10, 2 );
  */
 function red_starter_scripts() {
 	wp_enqueue_style( 'red-starter-style', get_stylesheet_uri() );
+<<<<<<< HEAD
 	wp_enqueue_style( 'big-sisters-font', 'https://fonts.googleapis.com/css?family=Roboto:700,500' );
+=======
+	wp_enqueue_style ( 'font-awesome-cdn' , 'https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css', array(), '4.4.0');
+
+
+>>>>>>> Build and style footer for mobile view.
 	wp_enqueue_script( 'red-starter-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array(), '20130115', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
