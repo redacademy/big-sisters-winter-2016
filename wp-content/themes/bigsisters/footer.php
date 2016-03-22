eb2e04519228fd5d33b053fee0414b7e341ce04f
Jeremiah Almazar <jemalmazar@gmail.com>
2016-03-21 23:46:37 -0700

    Build and style footer for mobile view.

--- wp-content/themes/bigsisters/footer.php
+++ wp-content/themes/bigsisters/footer.php
@@ -10,8 +10,31 @@
 			</div><!-- #content -->
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
-				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
+				<div class="site-info container">
+
+					<div class="contact">
+						<h2><a href="#" class="contact-button">Contact Us</a></h2>
+						<div class="address">
+							<p>34 E. 12th Avenue</p>
+							<p>Vancouver, BC</p>
+							<p>V5T 2G5</p>
+						</div>
+						<div class="phone-fax-email">
+							<p>P: 604.873.4525</p>
+							<p>F: 604.873.2122</p>
+							<p>E: <a href="mailto:info@bigsisters.bc.ca">info@bigsisters.bc.ca</a></p>
+						</div>
+					</div>
+
+					<div class="follow">
+						<h2>Follow Us:</h2>
+						<div class="social-icons">
+							<i class="fa fa-facebook"></i>
+							<i class="fa fa-twitter"></i>
+							<i class="fa fa-instagram"></i>
+						</div>
+					</div>
+
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
