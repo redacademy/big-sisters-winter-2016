9d1c3dcfc3d9ceed74f81db9245f1bd07910e110
Alan Chow <hello@alanchow.ca>
2016-03-24 13:51:48 -0700

    add initiative blocks

--- wp-content/themes/bigsisters/archive-story.php
+++ wp-content/themes/bigsisters/archive-story.php
@@ -58,15 +58,55 @@ get_header(); ?>
 								<!-- Do special_cat stuff... -->
 								<a href="<?php the_permalink() ?>"><p><?php the_title(); ?></p></a>
 							<?php endwhile; ?>
-							<!-- <?php while ( have_posts() ) : the_post(); ?>
-							<a href="<?php the_permalink() ?>"><p><?php the_title(); ?></p></a>
-						<?php endwhile; ?> -->
 					<?php else : ?>
 						<?php get_template_part( 'template-parts/content', 'none' ); ?>
 					<?php endif; ?>
 				</div>
 			</div>
 		</div>
+
+		<div class="initiative-block">
+			<div class="initiative-logo">
+				<img src="<?php echo get_template_directory_uri() ?>/assets/images/initiatives/big-sisters-mentor.jpg" alt="Become a Big Sisters Mentor" />
+		</div>
+		<div class="initiative-cta big-sister-mentor">
+			<h2>Big Sisters</h2>
+			<p>
+				2 - 4 Hours/Week<br />
+				1 Year Commitment
+			</p>
+			<button class="button-white">Learn More</button>
+		</div>
+	</div>
+
+	<div class="initiative-block">
+		<div class="initiative-logo">
+			<img src="<?php echo get_template_directory_uri() ?>/assets/images/initiatives/study-buddy-mentor.jpg" alt="Become a Study Buddy" />
+	</div>
+	<div class="initiative-cta study-buddy-mentor">
+		<h2>Study Buddy</h2>
+		<p>
+			1 Hour/Week<br />
+			6 Month Commitment
+		</p>
+		<button class="button-white">Learn More</button>
+	</div>
+</div>
+
+<div class="initiative-block">
+	<div class="initiative-logo">
+		<img src="<?php echo get_template_directory_uri() ?>/assets/images/initiatives/go-girls-mentor.jpg" alt="Become a Go Girls Mentor" />
+</div>
+<div class="initiative-cta go-girls-mentor">
+	<h2>Go Girls</h2>
+	<p>
+		2 Hours/Week<br />
+		8 Week Commitment
+	</p>
+	<button class="button-white">Learn More</button>
+</div>
+</div>
+
 
 </main><!-- #main -->
 </div><!-- #primary -->
