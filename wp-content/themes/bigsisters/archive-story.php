842ace051780f897e2be66c96e0796057e1d34b4
Alan Chow <hello@alanchow.ca>
2016-03-26 00:27:13 -0700

    slight update to success stories

--- wp-content/themes/bigsisters/archive-story.php
+++ wp-content/themes/bigsisters/archive-story.php
@@ -16,6 +16,8 @@ get_header(); ?>
 				the_archive_title( '<h1 class="page-title">', '</h1>' );
 				?>
 			</header><!-- .page-header -->
+
+			<section class="success-section">
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 				<?php if ( has_post_thumbnail() ) : ?>
@@ -64,49 +66,11 @@ get_header(); ?>
 				</div>
 			</div>
 		</div>
+</section>
 
-		<div class="initiative-block">
-			<div class="initiative-logo">
-				<img src="<?php echo get_template_directory_uri() ?>/assets/images/initiatives/big-sisters-mentor.jpg" alt="Become a Big Sisters Mentor" />
-		</div>
-		<div class="initiative-cta big-sister-mentor">
-			<h2>Big Sisters</h2>
-			<p>
-				2 - 4 Hours/Week<br />
-				1 Year Commitment
-			</p>
-			<button class="button-white">Learn More</button>
-		</div>
-	</div>
-
-	<div class="initiative-block">
-		<div class="initiative-logo">
-			<img src="<?php echo get_template_directory_uri() ?>/assets/images/initiatives/study-buddy-mentor.jpg" alt="Become a Study Buddy" />
-	</div>
-	<div class="initiative-cta study-buddy-mentor">
-		<h2>Study Buddy</h2>
-		<p>
-			1 Hour/Week<br />
-			6 Month Commitment
-		</p>
-		<button class="button-white">Learn More</button>
-	</div>
-</div>
-
-<div class="initiative-block">
-	<div class="initiative-logo">
-		<img src="<?php echo get_template_directory_uri() ?>/assets/images/initiatives/go-girls-mentor.jpg" alt="Become a Go Girls Mentor" />
-</div>
-<div class="initiative-cta go-girls-mentor">
-	<h2>Go Girls</h2>
-	<p>
-		2 Hours/Week<br />
-		8 Week Commitment
-	</p>
-	<button class="button-white">Learn More</button>
-</div>
-</div>
-
+<section class="initatives-sect">
+<?php include_once('inc/initatives.php'); ?>
+</section>
 
 </main><!-- #main -->
 </div><!-- #primary -->
