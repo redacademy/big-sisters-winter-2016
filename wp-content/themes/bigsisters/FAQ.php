bc86b7714bd21aa22348abbe247239df6d706c5d
tylere5 <ewanchuk.tyler@gmail.com>
2016-03-26 12:29:31 -0700

    Created faq.php and added content

---
+++ wp-content/themes/bigsisters/FAQ.php
@@ -0,0 +1,148 @@
+<?php
+/**
+ * Template Name: FAQ
+ **/
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+    <section class="container">
+
+        <h1>Frequently Asked Questions</h1>
+
+        <h2>ABOUT THE PROGRAM</h2>
+
+        <div class="accordian-box">
+          <div class="acr-box-div">
+            <div class="info-panel-content">
+              <p>What is the Go Girls Program</p>
+              <i class="fa fa-plus-circle"></i>
+            </div>
+            <div class="acr-box-content container">
+              Duis dapibus non tortor vitae congue.
+              Donec ut facilisis leo. Donec tincidunt condimentum ex, eu fermentum
+              felis venenatis non. Sed molestie dapibus tellus, id ultricies tellus
+              tempus ac. Nam nunc est, vehicula sit amet pretium ac, accumsan non
+              justo. Curabitur justo lectus, sodales at placerat vitae,
+              facilisis vel leo.
+            </div>
+          </div>
+
+          <div class="acr-box-div">
+            <div class="info-panel-content">
+              <p>What is Big Sisters?</p>
+              <i class="fa fa-plus-circle"></i>
+            </div>
+            <div class="acr-box-content container">
+              Duis dapibus non tortor vitae congue.
+              Donec ut facilisis leo. Donec tincidunt condimentum ex, eu fermentum
+              felis venenatis non. Sed molestie dapibus tellus, id ultricies tellus
+              tempus ac. Nam nunc est, vehicula sit amet pretium ac, accumsan non
+              justo. Curabitur justo lectus, sodales at placerat vitae,
+              facilisis vel leo.
+            </div>
+          </div>
+
+          <div class="acr-box-div">
+            <div class="info-panel-content">
+              <p>What king of support can I expect after joining Big Sisters?</p>
+              <i class="fa fa-plus-circle"></i>
+            </div>
+            <div class="acr-box-content container">
+              Duis dapibus non tortor vitae congue.
+              Donec ut facilisis leo. Donec tincidunt condimentum ex, eu fermentum
+              felis venenatis non. Sed molestie dapibus tellus, id ultricies tellus
+              tempus ac. Nam nunc est, vehicula sit amet pretium ac, accumsan non
+              justo. Curabitur justo lectus, sodales at placerat vitae,
+              facilisis vel leo.
+            </div>
+          </div>
+
+          <div class="acr-box-div">
+            <div class="info-panel-content">
+              <p>When can I see my Little Sister?</p>
+              <i class="fa fa-plus-circle"></i>
+            </div>
+            <div class="acr-box-content container">
+              Duis dapibus non tortor vitae congue.
+              Donec ut facilisis leo. Donec tincidunt condimentum ex, eu fermentum
+              felis venenatis non. Sed molestie dapibus tellus, id ultricies tellus
+              tempus ac. Nam nunc est, vehicula sit amet pretium ac, accumsan non
+              justo. Curabitur justo lectus, sodales at placerat vitae,
+              facilisis vel leo.
+            </div>
+          </div>
+        </div>
+
+        <h2>HOSTING A FUNDRAISER</h2>
+
+        <div class="accordian-box">
+          <div class="acr-box-div">
+            <div class="info-panel-content">
+              <p>Can I claim taxes back on donation?</p>
+              <i class="fa fa-plus-circle"></i>
+            </div>
+            <div class="acr-box-content container">
+              Duis dapibus non tortor vitae congue.
+              Donec ut facilisis leo. Donec tincidunt condimentum ex, eu fermentum
+              felis venenatis non. Sed molestie dapibus tellus, id ultricies tellus
+              tempus ac. Nam nunc est, vehicula sit amet pretium ac, accumsan non
+              justo. Curabitur justo lectus, sodales at placerat vitae,
+              facilisis vel leo.
+            </div>
+          </div>
+
+          <div class="acr-box-div">
+            <div class="info-panel-content">
+              <p>Will my donation make a difference?</p>
+              <i class="fa fa-plus-circle"></i>
+            </div>
+            <div class="acr-box-content container">
+              Duis dapibus non tortor vitae congue.
+              Donec ut facilisis leo. Donec tincidunt condimentum ex, eu fermentum
+              felis venenatis non. Sed molestie dapibus tellus, id ultricies tellus
+              tempus ac. Nam nunc est, vehicula sit amet pretium ac, accumsan non
+              justo. Curabitur justo lectus, sodales at placerat vitae,
+              facilisis vel leo.
+            </div>
+          </div>
+
+          <div class="acr-box-div">
+            <div class="info-panel-content">
+              <p>How much does it cost to match a Big and Little?</p>
+              <i class="fa fa-plus-circle"></i>
+            </div>
+            <div class="acr-box-content container">
+              Duis dapibus non tortor vitae congue.
+              Donec ut facilisis leo. Donec tincidunt condimentum ex, eu fermentum
+              felis venenatis non. Sed molestie dapibus tellus, id ultricies tellus
+              tempus ac. Nam nunc est, vehicula sit amet pretium ac, accumsan non
+              justo. Curabitur justo lectus, sodales at placerat vitae,
+              facilisis vel leo.
+            </div>
+          </div>
+
+          <div class="acr-box-div">
+            <div class="info-panel-content">
+              <p>Can I make a corporate donation?</p>
+              <i class="fa fa-plus-circle"></i>
+            </div>
+            <div class="acr-box-content container">
+              Duis dapibus non tortor vitae congue.
+              Donec ut facilisis leo. Donec tincidunt condimentum ex, eu fermentum
+              felis venenatis non. Sed molestie dapibus tellus, id ultricies tellus
+              tempus ac. Nam nunc est, vehicula sit amet pretium ac, accumsan non
+              justo. Curabitur justo lectus, sodales at placerat vitae,
+              facilisis vel leo.
+            </div>
+          </div>
+        </div>
+
+    </section>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
