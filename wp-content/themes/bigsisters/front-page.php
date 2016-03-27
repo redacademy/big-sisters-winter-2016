181f317caaa65bac93571ae63c51dec7edfb2dfc
Amanda Wong <Amanda Wong>
2016-03-27 14:24:19 -0700

    Fix info-panel-style

--- wp-content/themes/bigsisters/front-page.php
+++ wp-content/themes/bigsisters/front-page.php
@@ -9,7 +9,7 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-            <section class="hero-slider">
+            <section class="hero-slider">	
                 <ul class="bxslider">
                     <li class="bigsister-hero-fp">
                         <p class="hero-quote">
