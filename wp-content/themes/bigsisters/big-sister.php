91c6221132bbb37e7673de8db306aa7242f849f6
Alan Chow <hello@alanchow.ca>
2016-03-28 01:11:32 -0700

    create volunteer programs template

---
+++ wp-content/themes/bigsisters/big-sister.php
@@ -0,0 +1,108 @@
+<?php
+/**
+* Template Name: Big Sister Info
+**/
+get_header();?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+		<header class="container story-page-copy">
+			<?php the_title( '<h1>', '</h1>' ); ?>
+		</header>
+
+		<section class="container">
+			<div class="program-hero">
+				<div class="program-hero-img">
+					<img src="<?php echo CFS()->get('hero1_img')?>" />
+				</div>
+				<div class="program-quote container">
+					<?php echo CFS()->get('hero1_quote')?>
+				</div>
+			</div>
+		</section>
+
+		<section class="container">
+			<div class="program-hero">
+				<div class="program-hero-img">
+					<img src="<?php echo CFS()->get('hero2_img')?>" />
+				</div>
+				<div class="program-quote container">
+					<?php echo CFS()->get('hero2_quote')?>
+				</div>
+			</div>
+		</section>
+
+		<section class="container">
+			<div class="program-hero">
+				<div class="program-hero-img">
+					<img src="<?php echo CFS()->get('hero3_img')?>" />
+				</div>
+				<div class="program-quote container">
+					<?php echo CFS()->get('hero3_quote')?>
+				</div>
+			</div>
+		</section>
+
+		<!-- Step 1 -->
+		<section class="container">
+			<div class="accordian-box">
+				<div class="acr-box-div">
+					<div class="info-panel-content">
+						<h3>Step 1. General Information</h3>
+						<i class="fa fa-plus-circle"></i>
+					</div>
+					<div class="acr-box-content">
+						<?php echo CFS()->get('step_1')?>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- Step 2 -->
+		<section class="container">
+			<div class="accordian-box">
+				<div class="acr-box-div">
+					<div class="info-panel-content">
+						<h3>Step 2. Requirements</h3>
+						<i class="fa fa-plus-circle"></i>
+					</div>
+					<div class="acr-box-content">
+						<?php echo CFS()->get('step_2')?>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- Step 3 -->
+		<section class="container">
+			<div class="accordian-box">
+				<div class="acr-box-div">
+					<div class="info-panel-content">
+						<h3>Step 3. Apply Now!</h3>
+						<i class="fa fa-plus-circle"></i>
+					</div>
+					<div class="acr-box-content">
+						<?php echo CFS()->get('step_3')?>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- Step 4 -->
+		<section class="container">
+			<div class="accordian-box">
+				<div class="acr-box-div">
+					<div class="info-panel-content">
+						<h3>Step 4. Next Steps</h3>
+						<i class="fa fa-plus-circle"></i>
+					</div>
+					<div class="acr-box-content">
+						<?php echo CFS()->get('step_4')?>
+					</div>
+				</div>
+			</div>
+		</section>
+
+	</main> <!-- #main -->
+</div> <!-- #primary -->
+
+<!-- Other ways to be involved colour blocks -->
+
+<?php get_footer(); ?>
