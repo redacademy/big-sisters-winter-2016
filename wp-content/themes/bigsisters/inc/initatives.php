169535b3b1a109bf4733078aa675410bdd0a6302
Alan Chow <hello@alanchow.ca>
2016-03-29 11:06:37 -0700

    update css styles

--- wp-content/themes/bigsisters/inc/initatives.php
+++ wp-content/themes/bigsisters/inc/initatives.php
@@ -1,43 +1,43 @@
 <section class="volunteer-progs">
   <div class="initiative-block">
-  <div class="initiative-logo">
-    <img src="<?php echo get_template_directory_uri() ?>/assets/images/initiatives/big-sisters-mentor.jpg" alt="Become a Big Sisters Mentor" />
-  </div>
   <div class="initiative-cta big-sister-mentor">
     <h2 class="initiative-head">Big Sisters</h2>
     <p>
       2 - 4 Hours/Week<br />
       1 Year Commitment
     </p>
-    <button class="button-white">Learn More</button>
+    <a href="<?php echo get_template_directory_uri()?>/big-sisters"><button class="button-white">Learn More</button></a>
+  </div>
+  <div class="initiative-logo">
+    <img src="<?php echo get_template_directory_uri() ?>/assets/images/initiatives/big-sisters-mentor.jpg" alt="Become a Big Sisters Mentor" />
   </div>
 </div>
 
 <div class="initiative-block">
-  <div class="initiative-logo">
-    <img src="<?php echo get_template_directory_uri() ?>/assets/images/initiatives/study-buddy-mentor.jpg" alt="Become a Study Buddy" />
-  </div>
   <div class="initiative-cta study-buddy-mentor">
     <h2 class="initiative-head">Study Buddy</h2>
     <p>
       1 Hour/Week<br />
       6 Month Commitment
     </p>
-    <button class="button-white">Learn More</button>
+    <a href="<?php echo get_template_directory_uri()?>/study-buddy"><button class="button-white">Learn More</button></a>
+  </div>
+  <div class="initiative-logo">
+    <img src="<?php echo get_template_directory_uri() ?>/assets/images/initiatives/study-buddy-mentor.jpg" alt="Become a Study Buddy" />
   </div>
 </div>
 
 <div class="initiative-block">
-  <div class="initiative-logo">
-    <img src="<?php echo get_template_directory_uri() ?>/assets/images/initiatives/go-girls-mentor.jpg" alt="Become a Go Girls Mentor" />
-  </div>
   <div class="initiative-cta go-girls-mentor">
     <h2 class="initiative-head">Go Girls</h2>
     <p>
       2 Hours/Week<br />
       8 Week Commitment
     </p>
-    <button class="button-white">Learn More</button>
+    <a href="<?php echo get_template_directory_uri()?>/go-girls"><button class="button-white">Learn More</button></a>
+  </div>
+  <div class="initiative-logo">
+    <img src="<?php echo get_template_directory_uri() ?>/assets/images/initiatives/go-girls-mentor.jpg" alt="Become a Go Girls Mentor" />
   </div>
 </div>
 </section>
