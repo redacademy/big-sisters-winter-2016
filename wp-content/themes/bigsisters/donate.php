5ca6d95b66249745181893ae15b0bced4445124e
Amanda Wong <Amanda Wong>
2016-03-29 11:45:05 -0700

    Style donate-page both mobile and desktop view

--- wp-content/themes/bigsisters/donate.php
+++ wp-content/themes/bigsisters/donate.php
@@ -11,37 +11,35 @@ get_header(); ?>
                 <p>
                     It costs $2000/year to sponsor one Little Sister mentorship. Our  donors make this happen.
                 </p>
-                <button class="button-brightblue">Donate Now</button>
+                <a href="#"><button class="button-brightblue">Donate Now</button></a>
             </section>
-            <section>
+            <section class="why-donate">
                 <h1>Why Donte to Big Sisters</h1>
                 <p>
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                 </p>
             </section>
-            <ul class="donate-types">
-                <li>Current mentor resources</li>
-                <li>Corporate Giving</li>
-                <li>Host A Fundraiser Or Event</li>
-                <li>Host a big and little event</li>
-            </ul>
-            <section>
-                <h1>What to sponsor or donate to an event?</h1>
+            <div class="donate-types">
+                <a href="#"><div>Current mentor resources</div></a>
+                <a href="#"><div>Corporate Giving</div></a>
+                <a href="#"><div>Host A Fundraiser Or Event</div></a>
+                <a href="#"><div>Host a big and little event</div></a>
+            </div>
+            <section class="to-donate-sponsor">
+                <h1>Want to sponsor or donate to an event?</h1>
                 <p>Please contact our manager of Corporate and Foundation giving</p>
                 <div class="donor-contact">
-                    <span>Fleur Cooper</span>
-                    <span>604-873-4525 ext. 317</span>
+					<div class="name-number-wrap">
+                    	<span>Fleur Cooper</span>
+                    	<span>604-873-4525 ext. 317</span>
+					</div>
                     <span>Fcooper@bigsisters.bc.ca</span>
                 </div>
             </section>
-            <hr />
             <section>
                 <i class="fa fa-instagram"></i><h1>@BigSistersBCLM</h1>
                 <div class="instagram-images"></div>
             </section>
-
-
-
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
