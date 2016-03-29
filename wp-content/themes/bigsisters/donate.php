c587ab0eff8e9c384802e33d133014c690dfdc66
Amanda Wong <Amanda Wong>
2016-03-29 00:05:39 -0700

    Add donor content

---
+++ wp-content/themes/bigsisters/donate.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Template Name: Donate Page
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+            <section class="hero-dp">
+                <p>
+                    It costs $2000/year to sponsor one Little Sister mentorship. Our  donors make this happen.
+                </p>
+                <button class="button-brightblue">Donate Now</button>
+            </section>
+            <section>
+                <h1>Why Donte to Big Sisters</h1>
+                <p>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                </p>
+            </section>
+            <ul class="donate-types">
+                <li>Current mentor resources</li>
+                <li>Corporate Giving</li>
+                <li>Host A Fundraiser Or Event</li>
+                <li>Host a big and little event</li>
+            </ul>
+            <section>
+                <h1>What to sponsor or donate to an event?</h1>
+                <p>Please contact our manager of Corporate and Foundation giving</p>
+                <div class="donor-contact">
+                    <span>Fleur Cooper</span>
+                    <span>604-873-4525 ext. 317</span>
+                    <span>Fcooper@bigsisters.bc.ca</span>
+                </div>
+            </section>
+            <hr />
+            <section>
+                <i class="fa fa-instagram"></i><h1>@BigSistersBCLM</h1>
+                <div class="instagram-images"></div>
+            </section>
+
+
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
