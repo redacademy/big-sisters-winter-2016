6447f9179fb1bb816dfa34ad68443018d2e4d06a
tylere5 <ewanchuk.tyler@gmail.com>
2016-03-30 15:05:32 -0700

    Added flex to article section of c-m-r.php

--- wp-content/themes/bigsisters/current-mentor-resources.php
+++ wp-content/themes/bigsisters/current-mentor-resources.php
@@ -23,30 +23,38 @@ get_header(); ?>
 
 				<div class="mentor-article">
 					<img class="article-one-image" src="<?php echo CFS()->get( 'article_one_image' ); ?>">
-					<p class="event-date"><?php echo esc_html(CFS()->get( 'event_one_date' ) ); ?></br>
-																<?php echo esc_html(CFS()->get( 'event_one_title' ) ); ?></p>
-					<p class="event-description"><?php echo esc_html(CFS()->get( 'event_one_description' ) ); ?></p>
+						<div class="ma-content">
+							<p class="event-date"><?php echo esc_html(CFS()->get( 'event_one_date' ) ); ?></br>
+																		<?php echo esc_html(CFS()->get( 'event_one_title' ) ); ?></p>
+							<p class="event-description"><?php echo esc_html(CFS()->get( 'event_one_description' ) ); ?></p>
+						</div>
 				</div>
 
 				<div class="mentor-article">
 					<img class="article-two-image" src="<?php echo CFS()->get( 'article_two_image' ); ?>">
-					<p class="event-date"><?php echo esc_html(CFS()->get( 'event_two_date' ) ); ?><br/>
-																<?php echo esc_html(CFS()->get( 'event_two_title' ) ); ?></p>
-					<p class="event-description"><?php echo esc_html(CFS()->get( 'event_two_description' ) ); ?></p>
+						<div class="ma-content">
+							<p class="event-date"><?php echo esc_html(CFS()->get( 'event_two_date' ) ); ?><br/>
+																		<?php echo esc_html(CFS()->get( 'event_two_title' ) ); ?></p>
+							<p class="event-description"><?php echo esc_html(CFS()->get( 'event_two_description' ) ); ?></p>
+						</div>
 				</div>
 
 				<div class="mentor-article">
 					<img class="article-three-image" src="<?php echo CFS()->get( 'article_three_image' ); ?>">
-					<p class="event-date"><?php echo esc_html(CFS()->get( 'event_three_date' ) ); ?><br/>
-																<?php echo esc_html(CFS()->get( 'event_three_title' ) ); ?></p>
-					<p class="event-description"><?php echo esc_html(CFS()->get( 'event_three_description' ) ); ?></p>
+						<div class="ma-content">
+							<p class="event-date"><?php echo esc_html(CFS()->get( 'event_three_date' ) ); ?><br/>
+																		<?php echo esc_html(CFS()->get( 'event_three_title' ) ); ?></p>
+							<p class="event-description"><?php echo esc_html(CFS()->get( 'event_three_description' ) ); ?></p>
+						</div>
 				</div>
 
 				<div class="mentor-article">
 					<img class="article-four-image" src="<?php echo CFS()->get( 'article_four_image' ); ?>">
-					<p class="event-date"><?php echo esc_html(CFS()->get( 'event_four_date' ) ); ?></br>
-																<?php echo esc_html(CFS()->get( 'event_four_title' ) ); ?></p>
-					<p class="event-description"><?php echo esc_html(CFS()->get( 'event_four_description' ) ); ?></p>
+						<div class="ma-content">
+							<p class="event-date"><?php echo esc_html(CFS()->get( 'event_four_date' ) ); ?></br>
+																		<?php echo esc_html(CFS()->get( 'event_four_title' ) ); ?></p>
+							<p class="event-description"><?php echo esc_html(CFS()->get( 'event_four_description' ) ); ?></p>
+						</div>
 				</div>
 			</section>
 
