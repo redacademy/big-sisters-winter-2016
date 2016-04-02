5feb3bec3ba38400c6cbae4306dab00c8aab573d
tylere5 <ewanchuk.tyler@gmail.com>
2016-04-02 16:03:29 -0700

    Added hero image, and fixed HTML validation

--- wp-content/themes/bigsisters/ways-to-give.php
+++ wp-content/themes/bigsisters/ways-to-give.php
@@ -8,6 +8,21 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
+			<section class="ways2give-hero">
+				<p class="ways2give-quote">When you contribute to Big Sisters you're
+				helping a child reach their full potential.</p>
+				<button class="button-brightblue">LEARN MORE</button>
+			</section>
+
+			<section class="container">
+			  <h1 class="page-header-style">Our Donors make Big Sisters possible. Every little bit helps.</h1>
+				  <p>
+				    We undestand that every one is different. Which is why Big Sisters
+				    offers so many different ways to contribute to our cause. We
+				    sincerely thank you for considering donating to our organization.
+				  </p>
+			</section>
+
 			<section class="give-flex">
 				<ul>
 					<?php
@@ -24,216 +39,6 @@ get_header(); ?>
 				 </li>
 				 </ul>
 		  </section>
-
-			<!--<section class="ways2give-hero">
-				<p class="ways2give-quote">When you contribute to Big Sisters you're
-				helping a child reach their full potential.</p>
-				<button class="button-brightblue">LEARN MORE</button>
-			</section>
-
-      <section class="container">
-        <h1 class="page-header-style">Our Donors make Big Sisters possible. Every little bit helps.</h1>
-        <p>
-          We undestand that every one is different. Which is why Big Sisters
-          offers so many different ways to contribute to our cause. We
-          sincerely thank you for considering donating to our organization.
-        </p>
-      </section>
-
-      <section class="donate-option-sections">
-        <div class="donate-option odd-color">
-					<div class="container give-flex">
-          <div class="donate-icon">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/one-time-donation-icon.svg" alt="Big Sisters One Time Donation">
-          </div>
-
-          <div class="donate-option-description">
-            <h3>One-time Donation</h3>
-            <p>
-              Start something....today! You can make an online donation here. If
-              you prefer to make your donation over the phone please call
-              604-873-4525 x321 or fill out the online donation form here.
-            </p>
-          </div>
-				</div>
-        </div>
-
-        <div class="donate-option">
-					<div class="container give-flex">
-          <div class="donate-icon">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/monthly-donation-icon.svg" alt="Big Sisters Monthly Donation">
-          </div>
-
-          <div class="donate-option-description">
-            <h3>Monthly Donation</h3>
-            <p>
-              Help Big Sisters plan for the future by dedicating your donation
-              on a monthly basis. Your ongoing commitment to mentoring will impact
-              the girls we serve today and tomorrow. Set up your monthly donation
-              online, call 604-873-4525 x321, or download the donation form.
-            </p>
-          </div>
-					</div>
-        </div>
-
-        <div class="donate-option odd-color">
-					<div class="container give-flex">
-          <div class="donate-icon">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/united-way-icon.png" alt="Big Sisters United Way Donation">
-          </div>
-
-          <div class="donate-option-description">
-            <h3>United Way Donation</h3>
-            <p>
-              Big Sisters is a proud community partner of the United Way of the
-              Lower Mainland. By designating your United Way gift to Big Sisters,
-              you will be supporting Little Sisters in your community.
-            </p>
-          </div>
-					</div>
-        </div>
-
-        <div class="donate-option">
-					<div class="container give-flex">
-          <div class="donate-icon">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/tribute-gifts-icon.svg" alt="Big Sisters Tributes and Memorial Gifts">
-          </div>
-
-          <div class="donate-option-description">
-            <h3>Tribute & Memorial Gifts</h3>
-            <p>
-              At the next special occassion, honour a friend, mentor or family
-              member with a gift in their honour. If a loved one has recently
-              passed, you can slo make a gift to Big Sisters in their memory.
-              Loved ones or their family will receive a personalized note of the
-              donation made in their honour and a tax receipt will be sent to you.
-              Make a gift in honour of someone special online, call 604-873-4524 x321,
-              or download the donation form.
-            </p>
-          </div>
-					</div>
-        </div>
-
-        <div class="donate-option odd-color">
-					<div class="container give-flex">
-          <div class="donate-icon">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/matching-gift-icon.svg" alt="Big Sisters Matching Gift Donation">
-          </div>
-
-          <div class="donate-option-description">
-            <h3>Matching Gift</h3>
-            <p>
-              Many employers will match your donations to registered charities
-              like Big Sisters. In addition some employers will donate a specified
-              amount to charities you volunteer for. By taking a few minutes to
-              check with your HR department you might double the size of your
-              gift!
-            </p>
-          </div>
-					</div>
-        </div>
-
-        <div class="donate-option">
-					<div class="container give-flex">
-          <div class="donate-icon">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/gift-in-will-icon.svg" alt="Big Sisters Gift in Will Donation">
-          </div>
-
-          <div class="donate-option-description">
-            <h3>Leave a Gift in Your Will</h3>
-            <p>
-              You may have benefited from the support of a mentor or you may
-              have been that mentor. You can further this impact by making
-              a bequest in your will that will provide others with mentors
-              for years to come. Talk to your lawyer and family to advise
-              them of your intentions.
-            </p>
-          </div>
-					</div>
-        </div>
-
-        <div class="donate-option odd-color">
-					<div class="container give-flex">
-          <div class="donate-icon">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/charity-car-program-icon.svg" alt="Big Sisters Charity Car Program Donation">
-          </div>
-
-          <div class="donate-option-description">
-            <h3>Charity Car Program</h3>
-            <p>
-              Big Sisters is a charity beneficiary of the Charity Car Program!
-              This allows individuals to give away no-longer-wanted automobiles
-              to charity organizations. This program collects and sells donated
-              cars and distributes the money to the seller's designated charity -
-              Big Sisters! Find out more here.
-            </p>
-          </div>
-					</div>
-        </div>
-
-        <div class="donate-option">
-					<div class="container give-flex">
-          <div class="donate-icon">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/donate-car-canada-icon.svg" alt="Big Sisters Donate Car Canada Donation">
-          </div>
-
-          <div class="donate-option-description">
-            <h3>Donate A Car Canada</h3>
-            <p>
-            Donate A Car Canada accepts Vehicle Donations for Big Sisters of BC
-						Lower Mainland. We provide free towing in many areas across Canada,
-						or you can drop off your vehicle to maximize your donation. When
-						you donate your RV, boat, motorcycle or other vehicle to Big Sisters
-						of BC Lower Mainland through Donate A Car Canada, it will either be
-						recycled or sold at auction. Donate A Car Canada will look after
-						all the details to make it easy for your Charity to benefit. After
-						your Vehicle Donation is complete, Big Sisters of BC Lower Mainland
-						will send you a tax receipt within 45 days!
-            </p>
-          </div>
-					</div>
-        </div>
-
-        <div class="donate-option odd-color">
-					<div class="container give-flex">
-          <div class="donate-icon">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/gift-of-stocks-icon.svg" alt="Big Sisters Gift of Stocks Donation">
-          </div>
-
-          <div class="donate-option-description">
-            <h3>Gifts of Stocks and Securities</h3>
-            <p>
-							Gifting stocks or securities to Big Sisters of BC Lower Mainland
-							can give you great tax advantages while providing more Little
-							Sisters with the gift of a mentor. For more information contact
-							Treena Innes at 604-873-4525 x313. Download the Securities
-							Donation Form.
-            </p>
-          </div>
-					</div>
-        </div>
-
-        <div class="donate-option">
-					<div class="container give-flex">
-          <div class="donate-icon">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/endowment-fund-icon.svg" alt="Big Sisters Endowment Fund Donation">
-          </div>
-
-          <div class="donate-option-description">
-            <h3>Endowment Fund by Peter & Joanne Brown</h3>
-            <p>
-              Contributions to our endowment fund (held with the Vancouver
-							Foundation will provide stable funding to our organization on a
-							long-term basis. For more information please contact Justine Green
-							at 604-873-4525 x307.
-            </p>
-          </div>
-					</div>
-        </div>
-
-      </section> -->
-
-
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
