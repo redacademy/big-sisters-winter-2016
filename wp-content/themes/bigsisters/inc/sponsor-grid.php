950068c4941e9a38953f5ad1a263bcf6b6bb163b
Alan Chow <hello@alanchow.ca>
2016-04-03 20:05:37 -0700

    reprocess scss files

--- wp-content/themes/bigsisters/inc/sponsor-grid.php
+++ wp-content/themes/bigsisters/inc/sponsor-grid.php
@@ -8,7 +8,7 @@
     <?php $logo_image_ID = $row['sponsor_logo']; ?>
     <?php $logo_image_ALT = get_post_meta($logo_image_ID, '_wp_attachment_image_alt', true); ?>
     <?php $logo_image_URL_data = wp_get_attachment_metadata($logo_image_ID, true); ?>
-    <?php $logo_image_URL = $logo_image_URL_data["file"]; ?>
+    <?php $logo_image_URL = $logo_image_URL_data['file']; ?>
     <li>	<img src="<?php echo $upload_path . $logo_image_URL; ?>" alt="<?php echo get_post_meta($row['sponsor_logo'], '_wp_attachment_image_alt', true)?>" /></li>
   <?php endforeach; ?>
 </ul>
