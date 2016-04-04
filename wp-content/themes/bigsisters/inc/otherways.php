60fc31a5c9a553003d6761140d15d72f239c1f55
Alan Chow <hello@alanchow.ca>
2016-04-03 22:16:07 -0700

    update donation links

--- wp-content/themes/bigsisters/inc/otherways.php
+++ wp-content/themes/bigsisters/inc/otherways.php
@@ -1,8 +1,8 @@
 <section class="other-way-blocks">
   <ul class="other-ways">
     <li><a href="<?php get_template_directory_uri();?>/donate" class="other-way-links">Other Ways To Donate</a></li>
-    <li><a href="<?php get_template_directory_uri();?>/host-a-fundraising-event" class="other-way-links">Host A Fundraiser Or Event</a></li>
-    <li><a href="<?php get_template_directory_uri();?>/host-a-big-and-little-event" class="other-way-links">Host A Big And Little Sister Activity</a></li>
+    <li><a href="<?php get_template_directory_uri();?>/host-a-fundraiser" class="other-way-links">Host A Fundraiser Or Event</a></li>
+    <li><a href="<?php get_template_directory_uri();?>/host-a-big-little-event" class="other-way-links">Host A Big And Little Sister Activity</a></li>
     <li><a href="<?php get_template_directory_uri();?>/donate" class="other-way-links">Corporate Giving</a></li>
   </ul>
 </section>
