d9312a877e577b76b3274c043bd55ec887d9f0b6
Jeremiah Almazar <jemalmazar@gmail.com>
2016-04-05 15:44:00 -0700

    Remove container class wrapping entire events archive page.

--- wp-content/themes/bigsisters/archive-events.php
+++ wp-content/themes/bigsisters/archive-events.php
@@ -7,7 +7,7 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area container">
+	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
 		<div class="events">
