94b6c227a7136f72ebd8f11e97796e6608e41d3c
Jeremiah Almazar <jemalmazar@gmail.com>
2016-04-05 11:58:54 -0700

    Build and style event archive and single pages.

--- wp-content/themes/bigsisters/archive-events.php
+++ wp-content/themes/bigsisters/archive-events.php
@@ -10,31 +10,104 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<?php if ( have_posts() ) : ?>
+		<div class="events">
 
-			<header class="page-header">
-				<?php
-					the_archive_title( '<h1 class="page-header-style">', '</h1>' );
-					the_archive_description( '<div class="taxonomy-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+			<section class="premier-events container">
+				<?php if ( have_posts() ) : ?>
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+					<header class="page-header-style">
+						<h1>Premier Events</h1>
+					</header><!-- .page-header -->
 
-				<?php
-					get_template_part( 'template-parts/content', 'content' );
-				?>
+					<?php /* Start the Loop */ ?>
+					<?php query_posts( array( 'events_tax' => 'premier', 'showposts' => 3 ) ); ?>
+					<ul>
+						<?php while ( have_posts() ) : the_post(); ?>
+							<li>
+									<?php if (has_post_thumbnail() ) : ?>
+										<div class="event-thumbnail">
+											<div class="thumb-img-div">
+												<?php the_post_thumbnail('large'); ?>
+											</div>
+										</div> <!-- .event-thumbnail -->
+									<?php endif; ?>
+									<div class="event-content">
+										<h3> <?php the_title(); ?></h3>
+										<?php the_excerpt(); ?>
+										<a href="<?php the_permalink(); ?>">
+											<button class="learn-more button-white" type="button" name="learn-more">Learn More</button>
+										</a>
+									</div> <!-- .event-content -->
+							</li>
+						<?php endwhile; ?>
+					</ul>
 
-			<?php endwhile; ?>
+				<?php else : ?>
 
-			<?php the_posts_navigation(); ?>
+					<?php get_template_part( 'template-parts/content', 'none' ); ?>
 
-		<?php else : ?>
+				<?php endif; ?>
+			</section> <!-- .premier-events -->
 
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+			<section class="partner-events container">
+				<?php if ( have_posts() ) : ?>
 
-		<?php endif; ?>
+					<h2>Partner Events</h2>
+
+					<?php /* Start the Loop */ ?>
+					<?php query_posts( array( 'events_tax' => 'partner', 'showposts' => 2 ) ); ?>
+					<ul>
+						<?php while ( have_posts() ) : the_post(); ?>
+							<li>
+								<?php if (has_post_thumbnail() ) : ?>
+									<div class="event-thumbnail">
+										<div class="thumb-img-div">
+											<?php the_post_thumbnail('large'); ?>
+										</div>
+									</div> <!-- .event-thumbnail -->
+								<?php endif; ?>
+								<div class="event-content">
+									<h3> <?php the_title(); ?></h3>
+									<?php the_excerpt(); ?>
+									<a href="<?php the_permalink(); ?>">
+										<button class="learn-more button-white" type="button" name="learn-more">Learn More</button>
+									</a>
+								</div> <!-- .event-content -->
+							</li>
+						<?php endwhile; ?>
+					</ul>
+
+				<?php else : ?>
+
+					<?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+				<?php endif; ?>
+			</section> <!-- .partner-events -->
+
+			<section class="host-fundraiser-event">
+				<div class="container">
+					<h2>Want to host a fundraiser or event?</h2>
+					<a href="/bigsisters/host-a-fundraising-event/">
+						<button class="learn-more button-white" type="button" name="learn-more">Learn More</button>
+					</a>
+				</div>
+			</section>
+
+			<section class="to-donate-sponsor">
+				<h2>Want to sponsor or donate to an event?</h2>
+				<p>Please contact our manager of Corporate and Foundation giving</p>
+				<div class="donor-contact">
+					<div class="name-number-wrap">
+						<span>Fleur Cooper</span>
+						<span>604-873-4525 ext. 317</span>
+					</div> <!-- .name-number-up -->
+					<span>Fcooper@BigSisters.bc.ca</span>
+				</div> <!-- .donor-contact -->
+			</section>
+
+			<hr>
+
+		</div> <!-- .events -->
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
