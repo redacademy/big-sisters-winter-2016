5dbeecf0d1fbd1c1859a7b21edbd9c701e010659
Alan Chow <hello@alanchow.ca>
2016-08-05 00:22:34 -0700

    fix link paths

--- wp-content/themes/bigsisters/inc/otherways.php
+++ wp-content/themes/bigsisters/inc/otherways.php
@@ -1,7 +1,7 @@
 <section class="other-way-blocks">
   <ul class="other-ways">
-    <li><a href="<?php get_template_directory_uri();?>/ways-to-give" class="other-way-links">Other Ways To Donate</a></li>
-    <li>Corporate Giving</li>
+    <li><a href="<?php get_template_directory_uri();?>/donate/ways-to-give" class="other-way-links">Other Ways To Donate</a></li>
+    <li><a href="<?php get_template_directory_uri();?>/donate/corporate-giving" class="other-way-links">Corporate Giving</a></li>
     <li><a href="<?php get_template_directory_uri();?>/host-a-fundraising-event" class="other-way-links">Host A Fundraiser Or Event</a></li>
     <li><a href="<?php get_template_directory_uri();?>/host-a-big-and-little-event" class="other-way-links">Host A Big And Little Event</a></li>
   </ul>
