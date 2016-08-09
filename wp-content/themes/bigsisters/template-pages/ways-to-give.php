17419dc83cf01e0ca6c23fdc38ac36c312abd567
Amanda Wong <Amanda Wong>
2016-08-08 20:54:24 -0700

    Add donation slider to ways-to give template

--- wp-content/themes/bigsisters/template-pages/ways-to-give.php
+++ wp-content/themes/bigsisters/template-pages/ways-to-give.php
@@ -28,18 +28,32 @@ get_header(); ?>
 					<?php
 						$loop = CFS()->get('ways_to_give');
 						foreach($loop as $row) : ?>
-					 <li><div class="container">
-						 <div class="donate-icon"><img src="<?php echo $row['donate_icon']; ?>" alt="<?php echo esc_html(CFS()->get( 'donate_icon_description' ) ); ?>"/></div>
-						 <div class="donate-option-description">
-					 	<h2><?php echo $row['donate_title']; ?></h2>
-					 	<p><?php echo $row['donation_description']; ?></p>
-				 </div>
-			 </div>
-					 <?php endforeach; ?>
-				 </li>
+					<li>
+						<div class="container">
+						<div class="donate-icon"><img src="<?php echo $row['donate_icon']; ?>" alt="<?php echo esc_html(CFS()->get( 'donate_icon_description' ) ); ?>"/></div>
+						<div class="donate-option-description">
+					 		<h2><?php echo $row['donate_title']; ?></h2>
+					 		<p><?php echo $row['donation_description']; ?></p>
+				 		</div>
+			 			</div>
+					<?php endforeach; ?>
+				 	</li>
 				 </ul>
-		  </section>
-
+		  	</section>
+			<section class="fp-recent-donors">
+				<h1>Thank you to our recent donors</h1>
+				<ul class="main-carousel">
+					<?php $fields = CFS()->get( 'donation_information' );
+					foreach ( $fields as $field ) : ?>
+					<li class="carousel-cell individual-donor">
+					    <h3 class="donor-name"><?php echo $field['donor_name']; ?></h3>
+						<p class="donation-amount"><?php echo $field['donation_amount']; ?></p>
+						<p class="program-type"><?php echo $field['program_name']; ?></p>
+						<p class="sponsor-level"><?php echo $field['additional_information']; ?></p>
+					</li>
+					<?php endforeach; ?>
+				</ul>
+			</section>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
