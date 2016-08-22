ccfd3720e6939d2196670f141ea05b3246ba0707
Amanda Wong <Amanda Wong>
2016-08-21 19:14:47 -0700

    Replace with contact 7 short code form

--- wp-content/themes/bigsisters/front-page.php
+++ wp-content/themes/bigsisters/front-page.php
@@ -52,14 +52,14 @@ $directory = get_template_directory_uri();
             </section>
             <section class="newsletter">
                 <h1>Sign Up for Big Sisters Newsletter</h1>
-                <form>
-					<div class="name-container">
-						<input type="text" placeholder="First name" />
-	                    <input type="text" placeholder="Last name" />
-					</div>
-					<input type="email" placeholder="Email address" />
-                    <a class="button-white" href="#">Sign Up</a>
-                </form>
+				<!-- <div class="name-container">
+					<input type="text" placeholder="First name" />
+                    <input type="text" placeholder="Last name" />
+				</div>
+				<input type="email" placeholder="Email address" />
+                <a class="button-white" href="#">Sign Up</a> -->
+				<?php echo do_shortcode( '[contact-form-7 id="144" title="Newsletter Sign-up Form"]' ); ?>
+
             </section>
 			<section class="tri-img-grid container">
 				<div class="tri-img">
